7472815be7d02ab904de23e1a71fbc89da53a761
Iana Vasylieva <iana.vasylieva@gmail.com>
2024-11-20 10:34:51 -0500

    Flask app to create PEACE json for brainreg and deepblink

---
+++ flask_app/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+from forms import BaseForm, DeepBlinkForm, BrainRegForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Operation forms dictionary
+OPERATION_FORMS = {
+    "deepblink": DeepBlinkForm,
+    "brainreg": BrainRegForm
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', operations=OPERATION_FORMS.keys())
+
+
+@app.route('/operation/<operation>', methods=['GET', 'POST'])
+def operation_form(operation):
+    if operation not in OPERATION_FORMS:
+        return f"Operation '{operation}' not supported", 404
+
+    form_class = OPERATION_FORMS[operation]
+    form = form_class()
+
+    if form.validate_on_submit():
+        # Gather data into a JSON file
+        data = {field.name: field.data for field in form if field.name not in ["submit", "csrf_token"]}
+        # Save the JSON data to a file
+        with open('/h20/CBI/Iana/json/operation_data.json', 'w') as f:
+            import json
+            json.dump(data, f)
+        return jsonify(data)
+
+    return render_template('form.html', form=form, operation=operation)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FieldList, FormField
+from wtforms.validators import DataRequired
+
+
+class BaseForm(FlaskForm):
+    input = StringField('Input Location', validators=[DataRequired()])
+    output = StringField('Output Location', validators=[DataRequired()])
+    # submit = SubmitField('Submit')
+
+
+class DeepBlinkForm(BaseForm):
+    operation = StringField('Operation', validators=[DataRequired()], default='deepblink')
+    signal_channel = StringField('Signal channel (number, starting with 0)', default='0')
+    resolution_level = StringField('Resolution level (number, starting with 0)', default='0')
+
+
+class BrainRegForm(BaseForm):
+    operation = StringField('Operation', validators=[DataRequired()], default='brainreg')
+    background_channel = StringField('Background channel (number, starting with 0)', default='0')
+    atlas = StringField('Atlas (atlas name from Brainglobe Atlas API)', default='allen_mouse_25um')
+    orientation = StringField('Orientation (three-letter string)', default='sal')
+    brain_geometry = StringField('Brain Geometry (full / hemisphere_l / hemisphere_r)', default='full')
