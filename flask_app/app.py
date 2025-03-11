be7fb56c5d1cf72d917c81852345cef057b77897
Iana Vasylieva <iana.vasylieva@gmail.com>
2025-03-11 16:42:37 -0400

    Add SLURM queue URL

--- flask_app/app.py
+++ flask_app/app.py
@@ -123,5 +123,30 @@ def operation_form(operation):
     return render_template('form.html', form=form, operation=operation)
 
 
+@app.route('/queue')
+def slurm_queue():
+    jobs = []
+
+    import subprocess
+    try:
+        result = subprocess.run(["squeue", "--format=%i %u %j %P %t %M %D"], capture_output=True, text=True)
+        lines = result.stdout.strip().split("\n")
+
+        for line in lines[1:]:  # Skip the first line (header)
+            job_id, user, job_name, partition, state, time, nodes = line.split(maxsplit=6)
+            jobs.append({
+                "Job ID": job_id,
+                "User": user,
+                "Job Name": job_name,
+                "Partition": partition,
+                "State": state,
+                "Time": time,
+                "Nodes": nodes
+            })
+    except:
+        print("ERROR: Couldn't get job list")
+    return render_template('queue.html', queue=jobs)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=1313, debug=True)
