771c3938699b1969affb6d377046033b738d54a5
Iana Vasylieva <iana.vasylieva@gmail.com>
2025-04-09 15:48:56 -0400

    Render Operation field as hidden in all forms

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, SubmitField, FieldList, FormField, BooleanField
+from wtforms import Form, StringField, SubmitField, FieldList, FormField, BooleanField, HiddenField
 from wtforms.validators import DataRequired
 
 
@@ -10,77 +10,67 @@ class BaseForm(FlaskForm):
 
 
 class DeepBlinkForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='deepblink', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='deepblink')
     with_dbscan = BooleanField('With DBSCAN?')
-    # signal_channel = StringField('Signal channel (number, starting with 0)', default='0')
-    # resolution_level = StringField('Resolution level (number, starting with 0)', default='0')
 
 
 class BrainRegForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='brainreg', render_kw={"disabled": True})
-    # background_channel = StringField('Background channel (number, starting with 0)', default='0')
+    operation = HiddenField('Operation', validators=[DataRequired()], default='brainreg')
     atlas = StringField('Atlas (atlas name from Brainglobe Atlas API)', default='allen_mouse_25um')
     orientation = StringField('Orientation (three-letter string)', default='sal')
     brain_geometry = StringField('Brain Geometry (full / hemisphere_l / hemisphere_r)', default='full')
 
 
 class CellFinderForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='cellfinder', render_kw={"disabled": True})
-    # signal_channel = StringField('Signal channel (number, starting with 0)', default='0')
-    # resolution_level = StringField('Resolution level (number, starting with 0)', default='0')
+    operation = HiddenField('Operation', validators=[DataRequired()], default='cellfinder')
 
 
 class AntsForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='ants', render_kw={"disabled": True})
-    # background_channel = StringField('Background channel (number, starting with 0)', default='0')
+    operation = HiddenField('Operation', validators=[DataRequired()], default='ants')
     atlas = StringField('Atlas (atlas name from Brainglobe Atlas API)', default='allen_mouse_25um')
     orientation = StringField('Orientation (three-letter string)', default='sal')
 
 
 class ContrastStretchForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='stretch_contrast', render_kw={"disabled": True})
-    # channel = StringField('Channel (number, starting with 0)', default='0')
-    # resolution_level = StringField('Resolution level (number, starting with 0)', default='0')
+    operation = HiddenField('Operation', validators=[DataRequired()], default='stretch_contrast')
 
 
 class IlastikForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='ilastik', render_kw={"disabled": True})
-    # channel = StringField('Channel (number, starting with 0)', default='0')
-    # resolution_level = StringField('Resolution level (number, starting with 0)', default='0')
+    operation = HiddenField('Operation', validators=[DataRequired()], default='ilastik')
     model_path = StringField('Model path', validators=[DataRequired()])
 
 
 class DBSCANForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='dbscan', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='dbscan')
     cell_candidates_path = StringField('Detected cells path', validators=[DataRequired()])
     epsilon = StringField('Maximum Distance in Cluster', validators=[DataRequired()], default=3)
     min_samples = StringField('Minimum Number of Samples in Cluster', validators=[DataRequired()], default=2)
 
 
 class CellposeForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='cellpose', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='cellpose')
     model = StringField('Model name (general, nuclei, cyto, cyto2 etc)', validators=[DataRequired()], default='general')
 
 
 class ResNetClassificationForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='resnet_classification', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='resnet_classification')
     cell_candidates_path = StringField('Detected cells path', validators=[DataRequired()])
     model_path = StringField('Model path', validators=[DataRequired()])
 
 
 class Unet3DForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='unet_3d', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='unet_3d')
     model = StringField('Model path', validators=[DataRequired()])
 
 
 class DeleteBGDetectionsForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='delete_background_detections', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='delete_background_detections')
     cell_candidates_path = StringField('Detected cells path', validators=[DataRequired()])
     fg_mask_path = StringField('Foreground mask path', validators=[DataRequired()])
 
 
 class TransformPointsForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='transform_points', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='transform_points')
     cells_path = StringField('Detected cells path', validators=[DataRequired()])
     registration_path = StringField('Path to registration folder', validators=[DataRequired()])
 
@@ -91,6 +81,6 @@ class MetaFieldForm(Form):
 
 
 class CombineWithMetadataForm(BaseForm):
-    operation = StringField('Operation', validators=[DataRequired()], default='combine_with_metadata', render_kw={"disabled": True})
+    operation = HiddenField('Operation', validators=[DataRequired()], default='combine_with_metadata')
     cells_path = StringField('Cells CSV path', validators=[DataRequired()])
     metadata = FieldList(FormField(MetaFieldForm), min_entries=1)
